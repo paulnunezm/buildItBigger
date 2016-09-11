066dacc25505d134816d5c2fb4f2a68aa874e6db
Paul Nunez <paulnunezm@gmail.com>
2016-09-11 15:32:42 -0400

    - Added JokesProvider Library - Implemented in main activity

---
+++ jokeprovider/src/main/java/com/example/JokesProvider.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class JokesProvider {
+  static final String jokes[] = {
+      "Can a kangaroo jump higher than a house? Of course, a house doesn’t jump at all.\n",
+      "Anton, do you think I’m a bad mother?\n \n My name is Paul.",
+      "My wife’s cooking is so bad we usually pray after our food.",
+      "I'd like to buy a new boomerang please. Also, can you tell me how to throw the old one away?\n",
+      "Police officer: \"Can you identify yourself, sir?\"\n \n Driver pulls out his mirror and says: \"Yes, it's me.\"\n",
+      "It is so cold outside I saw a politician with his hands in his own pockets.",
+      "Pessimist: \"Things just can't get any worse!\" \n \n Optimist: \"Nah, of course they can!\"\n",
+      "I wanted to grow my own food but I couldn’t get bacon seeds anywhere.\n"
+  };
+
+  public static String getJoke() {
+    return jokes[(int) Math.floor(Math.random() * (jokes.length - 1))];
+  }
+}
